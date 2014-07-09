06ed9f30a156c3389e9012416f387cfb2eb887b4
Justin Ales <justin.ales@gmail.com>
2014-07-09 15:37:16 +0100

    added new condition defintion script

---
+++ defineConditions.m
@@ -0,0 +1,22 @@
+%Condition definitions
+%Condition 1: Away consistent
+dopplerInfo(1).stimDuration     = 0.250; %approximate stimulus duration in seconds
+dopplerInfo(1).preStimDuration  = 0.5;  %Static time before stimulus change
+dopplerInfo(1).postStimDuration = 0;  %static time aftter stimulus change
+dopplerInfo(1).isi              = 1;     %Inter Stimulus Interval
+dopplerInfo(1).responseDuration = 1;    %Post trial window for waiting for a response
+
+dopplerInfo(1).visualVelocity = 200;   %Stimulus velocity in cm per second
+dopplerInfo(1).audioVelocity  = 200;
+dopplerInfo(1).speedOfSound   = 34000; %speed of sound in cm per second
+dopplerInfo(1).stimRadiusCm   = 1;    %stimulus size in cm;
+dopplerInfo(1).stimFreq       = 500;  %Audio Stimulus frequency in Hz
+dopplerInfo(1).rampDuration   = 32/1000; %Ramp duration is around 1 video frame 16 ms
+
+
+
+%Condition 2: towards consistent
+dopplerInfo(2) = dopplerInfo(1); %Use the same settings for condition 1
+%Change the velocity values
+dopplerInfo(2).visualVelocity = -200;   %Stimulus velocity in cm per second
+dopplerInfo(2).audioVelocity  = -200;
