562b3b902ad8f70c6cb4bae91ea3f7979a41c53c
Lab User <labuser@pspc-jma23-1.st-andrews.ac.uk>
2014-08-11 16:55:01 +0100

    analyze experiment script

--- analyzeExperiment.m
+++ analyzeExperiment.m
@@ -1,11 +1,28 @@
 %function []=analyzeExperiment(experimentData,dopplerInfo)
 
+fnames = uigetfile('*.mat','MultiSelect','on')
 
+
+for iName =1:length(fnames);
+   
+    tmpLoad =load(fnames{iName});
+    
+    if iName ==1
+       experimentData = tmpLoad.experimentData; 
+    else
+        
+    experimentData = [experimentData tmpLoad.experimentData];
+    end
+end
+    
+
+validTrialList = [experimentData(:).validTrial]
 validData = experimentData(validTrialList);
 
 %Let's get our condition list.  NB tricky use of [] to pull out vector
 conditionOrder = [experimentData(:).condNumber];
 %Let's loop over conditions and organize the data
+nConditions = max(conditionOrder);
 for iCond = 1:nConditions
     
     %This finds which trial correponds to this condition.
@@ -21,18 +38,83 @@ for iCond = 1:nConditions
 
     %Let's take a mean of the choice
     meanResponse(iCond)   = mean(perConditionData(iCond).responseList);
+    meanResponseTime(iCond)   = mean(perConditionData(iCond).responseTimes);
     %This is a naive calculation of standard error (assuming gaussian, when data is binomial,
     %but useful for prelimplotting 
     stdErrResponse(iCond) = std(perConditionData(iCond).responseList)/sqrt(length(perConditionData(iCond).responseList));
+    stdErrResponseTime(iCond) = std(perConditionData(iCond).responseTimes)/sqrt(length(perConditionData(iCond).responseTimes));
     
-    visualVelocity(iCond) = dopplerInfo(iCond).visualVelocity;
-    audioVelocity(iCond)  = dopplerInfo(iCond).audioVelocity;
+    visualVelocity(iCond) = experimentData(condIdx(1)).dopplerInfo.visualVelocity;
+    audioVelocity(iCond)  = experimentData(condIdx(1)).dopplerInfo.audioVelocity;
+    
+    
+    
 end
 
+%%
+% 
+% 
+% errorbar(visualVelocity,meanResponse,stdErrResponse);
 
-errorbar(visualVelocity,meanResponse,stdErrResponse);
+visVel = reshape(visualVelocity,9,4)
+audVel = reshape(audioVelocity,9,4)
+
+r = reshape(meanResponse,9,4)
+rStdE = reshape(stdErrResponse,9,4)
+rt= reshape(meanResponseTime,9,4)
+rtStdE = reshape(stdErrResponse,9,4)
+
+figure(1)
+clf;
+errorbar(visVel,r,rStdE);
+legend('con','+150','-150','inc')
+
+figure(2)
+clf;
+errorbar(visVel,rt,rtStdE);
+
+legend('con','+150','-150','inc')
+
+figure(3)
+clf;
+errorbar(r,rt,rtStdE);
+
+legend('con','+150','-150','inc')
+
+rDiff(:,1) = r(:,2) - r(:,1);
+rDiff(:,2) = r(:,3) - r(:,1);
+rDiff(:,3) = r(:,4) - r(:,1);
+figure(101)
+clf;
+plot(visVel(:,1:3),rDiff)
+legend('+150','-150','inc')
+
+
+rtDiff(:,1) = rt(:,2) - rt(:,1);
+rtDiff(:,2) = rt(:,3) - rt(:,1);
+rtDiff(:,3) = rt(:,4) - rt(:,1);
+figure(102)
+clf;
+plot(visVel(:,1:3),rtDiff)
+legend('+150','-150','inc')
+% figure(102)
+% clf;
+% errorbar(visVel,rt,rtStdE);
+% 
+% legend('con','+150','-150','inc')
 
 
 
-
+% figure(101)
+% clf;
+% errorbar(audVel,r,rStdE);
+% legend('con','+150','-150','inc')
+% xlabel('AUDIO velocity')
+% 
+% figure(102)
+% clf;
+% errorbar(audVel,rt,rtStdE);
+% 
+% legend('con','+150','-150','inc')
+% xlabel('AUDIO velocity')
 
