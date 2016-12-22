c54b79a2b82661dbae27b8112447db9d59ac5811
Justin Ales <justin.ales@gmail.com>
2016-12-22 11:50:14 +0000

    removed not needed files

--- demo/createPureTone.m
+++
@@ -1,5 +0,0 @@
-function [tone] = createPureTone(samplingRate,freq,duration)
-% create a pure tone sound
-t=linspace(0,duration,round(samplingRate*duration));
-tone = sin(2*pi*freq*t);
-tone = [tone;tone];
